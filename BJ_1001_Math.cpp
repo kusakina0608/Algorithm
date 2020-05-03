c716e905e429b5a1582ce16244e0c029e6fa4a4c
Kina <platina.kusakina@gmail.com>
2020-05-03 23:29:35 +0900

    Create BJ_1001_Math.cpp

---
+++ BJ_1001_Math.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Kina on 2020/05/03.
+//
+#include <iostream>
+using namespace std;
+
+int main(void){
+    auto a = 2147483647;
+    auto b = -2147483648;
+
+    cin>>a>>b;
+    cout<<a-b<<endl;
+
+    return 0;
+}
