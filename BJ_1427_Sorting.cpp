3850e4ffbb24072685a595d3d8aca57ae2fc5712
Kina <platina.kusakina@gmail.com>
2020-05-04 00:03:15 +0900

    Create BJ_1427_Sorting.cpp

---
+++ BJ_1427_Sorting.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Kina on 2020/05/04.
+//
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    // TODO change to C++ style
+    char s[] = "1000000000";
+    int n_digit, i, j;
+    int d[10] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+    scanf(" %s", s);
+
+    n_digit = strlen(s);
+
+    for(i=0; i<n_digit; i++){
+        d[i] = s[i]-'0';
+    }
+
+    for(i=0; i<n_digit-1; i++){
+        for(j=0; j<n_digit-i; j++){
+            if(d[j]<d[j+1]){
+                d[j]=d[j]^d[j+1];
+                d[j+1]=d[j]^d[j+1];
+                d[j]=d[j]^d[j+1];
+            }
+        }
+    }
+
+    for(i=0; i<n_digit; i++){
+        s[i] = d[i]+'0';
+    }
+    s[i] = 0;
+
+    printf("%s",s);
+
+    return 0;
+}
