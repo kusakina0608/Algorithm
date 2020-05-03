fecc89e7df2ddae646dfdcd7d0402773e3032f06
Kina <platina.kusakina@gmail.com>
2020-05-03 23:33:01 +0900

    Create BJ_1008_Math.cpp

---
+++ BJ_1008_Math.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Kina on 2020/05/03.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void){
+    int a = 0;
+    int b = 0;
+
+    cin>>a>>b;
+    cout<<setprecision(10)<<a/(double)b<<endl;
+
+    return 0;
+}
