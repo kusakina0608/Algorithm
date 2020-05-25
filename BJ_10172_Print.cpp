58404e59ef612b6323cae44c51e97bb86bcbd21a
Kina <platina.kusakina@gmail.com>
2020-05-25 16:48:06 +0900

    Create BJ_10172_Print.cpp

---
+++ BJ_10172_Print.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Kina on 2020/02/08.
+//
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<"|\\_/|\n"<<"|q p|   /}\n"<<"( 0 )\"\"\"\\\n"<<"|\"^\"`    |\n"<<"||_/=\\\\__|";
+
+    return 0;
+}
