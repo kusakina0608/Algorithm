ba104e66c8e830aefdf7c4709a5434bb23bc9049
Kina <platina.kusakina@gmail.com>
2020-05-25 18:50:50 +0900

    Create SWEA_D1_2029.cpp

---
+++ SWEA_D1_2029.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Kina on 2020/02/15.
+//
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int test_case;
+    int T;
+    cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case){
+        int a, b;
+        cin>>a>>b;
+        cout<<'#'<<test_case<<' '<<a/b<<' '<<a%b<<endl;
+    }
+    return 0;
+}
