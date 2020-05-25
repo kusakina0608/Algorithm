8b0e6563a17f1d7bc71ffc618ca634e054019f66
Kina <platina.kusakina@gmail.com>
2020-05-25 16:49:25 +0900

    Create BJ_10718_Print.cpp

---
+++ BJ_10718_Print.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Kina on 2020/02/08.
+//
+#include <iostream>
+using namespace std;
+
+int main(void){
+    for(int i=0; i<2; i++)
+        cout<<"강한친구 대한육군"<<endl;
+    return 0;
+}
