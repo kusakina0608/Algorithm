687ec58d2f973ff7e9576e16e9cbc7fb7d402559
Kina <platina.kusakina@gmail.com>
2020-05-25 15:15:34 +0900

    Create BJ_2557_Print.cpp

---
+++ BJ_2557_Print.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Kina on 2020/02/08.
+//
+#include <iostream>
+int main(void){
+    std::cout<<'H'<<'e';
+    for(int i=0; i<2; i++)
+        std::cout<<'l';
+    if(true) std::cout<<'o'<<' ';
+    std::cout<<"World!";
+}
