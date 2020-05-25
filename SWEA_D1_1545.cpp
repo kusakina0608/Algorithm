70bae87fa8f0b9633f3642d1f13a7f746a1d45fe
Kina <platina.kusakina@gmail.com>
2020-05-25 18:50:39 +0900

    Create SWEA_D1_1545.cpp

---
+++ SWEA_D1_1545.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Kina on 2020/02/15.
+//
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int n;
+    cin>>n;
+    for(int i=n; i>=0; i--){
+        cout<<i<<' ';
+    }
+    return 0;
+}
