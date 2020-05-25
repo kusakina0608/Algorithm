28d72e6d7a6d0a78f653319c9505b876213d0cad
Kina <platina.kusakina@gmail.com>
2020-05-25 18:50:44 +0900

    Create SWEA_D1_1938.cpp

---
+++ SWEA_D1_1938.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Kina on 2020/02/15.
+//
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl;
+    return 0;
+}
