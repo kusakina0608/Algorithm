a8753f288e915905d08e43161dcb3388231f2be0
Kina <platina.kusakina@gmail.com>
2020-05-25 17:59:26 +0900

    Create BJ_10951_Math.cpp

---
+++ BJ_10951_Math.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Kina on 2020/02/08.
+//
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a, b;
+    while(cin>>a>>b){
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
