e9f0d0d68006a36367d3ace1adc7238b12669605
Kina <platina.kusakina@gmail.com>
2020-09-08 09:15:10 +0900

    Update SWEA_D2_1284.cpp

--- SWEA_D2_1284.cpp
+++ SWEA_D2_1284.cpp
@@ -1,3 +1,29 @@
-// TODO()
+//
 // Created by Kina on 2020/08/05.
 //
+#include<iostream>
+using namespace std;
+
+int T, test_case, P, Q, R, S, W;
+
+int fee_A(int amount) {
+    return P * amount;
+}
+
+int fee_B(int amount) {
+    return amount < R ? Q : ((amount - R)*S) + Q;
+}
+
+
+int main(int argc, char** argv) {
+    int test_case;
+    int T;
+    cin >> T;
+    for (test_case = 1; test_case <= T; ++test_case) {
+        cin >> P >> Q >> R >> S >> W;
+        int a = fee_A(W);
+        int b = fee_B(W);
+        cout << '#' << test_case << ' ' << (a < b ? a : b) << '\n';
+    }
+    return 0;
+}
